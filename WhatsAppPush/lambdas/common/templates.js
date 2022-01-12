5ec124e491e03856572b694b0fdc772ada99c17c
Emil Krzaczyński <emil.krzakuou@gmail.com>
2022-01-12 13:07:11 +0100

    Create endpoints and common directorys with Basic responses. Add webpack to the project

---
+++ WhatsAppPush/lambdas/common/templates.js
@@ -0,0 +1,29 @@
+const status = require('http-status')
+
+const Responses = {
+  _200(data = {}) {
+    return {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Origin': '*',
+      },
+      statusCode: status.SUCCESSFUL,
+      body: JSON.stringify(data),
+    }
+  },
+
+  _400(data = {}) {
+    return {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Origin': '*',
+      },
+      statusCode: status.CLIENT_ERROR,
+      body: JSON.stringify(data),
+    }
+  }
+}
+
+module.exports = Responses
